2e59307896db4720ed756785698ff9d4f6f62313
Matt Waite <matt.waite@gmail.com>
2016-03-18 15:54:26 -0500

    Updating scraper for new bugs

--- scraper.py
+++ scraper.py
@@ -12,23 +12,29 @@ uls = soup.find_all('ul')
 links = uls[9].find_all('a')
 
 with open('unlcrime.csv', 'a', encoding="utf-8") as csvfile:
-    for link in links:
-        detail = urllib.request.urlopen('%s%s' % (root_url, link['href']))
-        detail_soup = BeautifulSoup(detail, 'html.parser')
-        tds = detail_soup.find_all('td')
-        inc = tds[3].text
-        incident = inc.replace('Incident Number:', '').lstrip(' ')
-        rep = tds[5].text
-        reported = rep.replace('Date/Time Reported:', '').lstrip(' ')
-        oc = tds[9].text
-        occurred = oc.replace('Occurred Date:', '').lstrip(' ')
-        building = tds[12].text
-        address = tds[14].text
-        campus = tds[16].text
-        city = tds[18].text
-        location_code = tds[20].text
-        state = tds[22].text
-        incident_code = tds[24].text
-        synopsis = detail_soup.find(id=re.compile('.*_Synopsis$')).text
-        spamwriter = csv.writer(csvfile)
-        spamwriter.writerow([incident, reported, occurred, building, address, campus, city, state, location_code, incident_code, synopsis])    
+    try: 
+        for link in links:
+            detail_url = link['href']
+            report_url = root_url+detail_url
+            detail = urllib.request.urlopen(report_url)
+            detail_soup = BeautifulSoup(detail, 'html.parser')
+            tds = detail_soup.find_all('td')
+            inc = tds[3].text
+            incident = inc.replace('Incident Number:', '').lstrip(' ')
+            rep = tds[5].text
+            reported = rep.replace('Date/Time Reported:', '').lstrip(' ')
+            oc = tds[9].text
+            occurred = oc.replace('Occurred Date:', '').lstrip(' ')
+            building = tds[12].text
+            address = tds[14].text
+            campus = tds[16].text
+            city = tds[18].text
+            location_code = tds[20].text
+            state = tds[22].text
+            incident_code = tds[24].text
+            synopsis = detail_soup.find(id=re.compile('.*_Synopsis$')).text
+            synopsis = synopsis.replace('\n','').replace('\r','') 
+            datawriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            datawriter.writerow([incident, reported, occurred, building, address, campus, city, state, location_code, incident_code, synopsis])    
+    except:
+        pass
